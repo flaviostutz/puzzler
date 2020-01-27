8b5abf713d934269a3584706cff3427ec10b312e
flaviostutz <flaviostutz@gmail.com>
2020-01-27 17:09:26 -0300

    piece body detector ok

---
+++ notebooks/modules/calc.py
@@ -0,0 +1,15 @@
+import math
+
+#calculate the angle at point [1] formed by edges with [0] and [2]
+def angle_between_points(points):
+    a = points[0]
+    b = points[1]
+    c = points[2]
+    
+    m1 = (b[1]-a[1])/(b[0]-a[0])
+    m2 = (c[1]-b[1])/(c[0]-b[0])
+
+    tgb = (m2-m1)/(1+m1*m2)
+    
+    return math.degrees(math.atan(tgb))
+
