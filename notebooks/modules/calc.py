166086d38c85287175bcb7ea1569f4c6f8a6b18d
flaviostutz <flaviostutz@gmail.com>
2020-02-20 18:12:31 -0300

    rotating body shape

--- notebooks/modules/calc.py
+++ notebooks/modules/calc.py
@@ -1,4 +1,6 @@
 import math
+import numpy as np
+import cv2
 
 #calculate the angle at point [1] formed by edges with [0] and [2]
 def angle_between_points(points):
@@ -18,3 +20,28 @@ def is_near(value1, value2, diff):
     err = abs(value1 - value2)
     return err<=diff
 
+
+#point (x,y) center (x,y) angle in degrees
+def rotate_point(point, center, angle):
+  s = math.sin(math.radians(angle))
+  c = math.cos(math.radians(angle))
+  point = np.array(point)
+
+  #translate point back to origin:
+  point[0] = point[0] - center[0]
+  point[1] = point[1] - center[1]
+
+  #rotate point
+  xnew = point[0] * c - point[1] * s
+  ynew = point[0] * s + point[1] * c
+
+  #translate point back:
+  point2 = [0,0]
+  point2[0] = xnew + center[0]
+  point2[1] = ynew + center[1]
+  return point2
+
+def rotate_image(image, center, angle):
+    rot_mat = cv2.getRotationMatrix2D(center,-angle,1.0)
+    new_image = cv2.warpAffine(image, rot_mat, (image.shape[0],image.shape[1]))
+    return new_image
